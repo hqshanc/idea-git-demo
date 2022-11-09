17f4f4ec2ee68e5ab4d152911eecaa488fc8e648
hqshanc <847132441@qq.com>
2022-11-09 21:58:49 +0800

    第一次提交

---
+++ src/main/java/com/example/ideagitdemo/IdeaGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ideagitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaGitDemoApplication.class, args);
+    }
+
+}
