4e6b640448b632e141d248517a4cad455077d818
hqshanc <847132441@qq.com>
2022-11-09 22:01:12 +0800

    第二次提交

--- src/main/java/com/example/ideagitdemo/IdeaGitDemoApplication.java
+++ src/main/java/com/example/ideagitdemo/IdeaGitDemoApplication.java
@@ -8,6 +8,7 @@ public class IdeaGitDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IdeaGitDemoApplication.class, args);
+        System.out.println("");
     }
 
 }
